d4c21475c504908641e55f9e4b81e026b89901da
real <real@freedomlayer.org>
2017-07-26 16:37:57 +0300

    Fixed error in calculation of num_towers.

--- network_coords/net_coords/src/bin/towers_scc.rs
+++ network_coords/net_coords/src/bin/towers_scc.rs
@@ -51,8 +51,9 @@ fn main() {
                 // Choose towers:
                 let seed: &[_] = &[experiment_seed,2,g,net_type,net_iter];
                 let mut towers_rng: StdRng = rand::SeedableRng::from_seed(seed);
-                let num_colors = g*8;
-                let num_towers = 1 + (2_u64.pow(g as u32) / (num_colors as u64)) as usize;
+                let num_colors = g*4;
+                let total_num_towers = (2_u64.pow(g as u32) as f64).sqrt() as usize;
+                let num_towers = 1 + (total_num_towers / num_colors);
                 let chosen_towers = choose_towers(&net, num_towers, num_colors, &mut towers_rng);
                 // let chosen_towers = choose_towers(&net, 1, num_colors, &mut towers_rng); // Sanity check
                 let towers_info = calc_towers_info(&net, &chosen_towers);
