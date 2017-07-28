85acc0cfe5d541b3333dd6a2c219cf8cfeb21190
real <real@freedomlayer.org>
2017-07-28 14:15:51 +0300

    Checking variance of approx_size_harmonic.

--- approximate_net/src/main.rs
+++ approximate_net/src/main.rs
@@ -2,13 +2,32 @@ extern crate rand;
 extern crate approximate_net;
 
 use self::rand::{StdRng};
+// use self::rand::distributions::{IndependentSample, Range};
 use approximate_net::{gen_elems, calc_mins, approx_size_harmonic};
 
 fn main() {
-    let seed: &[_] = &[1,2,3,4,5];
+    let seed: &[_] = &[1,2,3,4,5,6];
     let mut rng: StdRng = rand::SeedableRng::from_seed(seed);
-    let elems = gen_elems(200000, &mut rng);
-    let mins = calc_mins(&elems, 50);
-    let approx_size = approx_size_harmonic(&mins);
-    println!("approx_size = {}", approx_size);
+    let num_mins = 40;
+    let num_iters = 100;
+    let num_elems = 10000;
+
+    let mut total_serror = 0;
+
+    for _ in 0 .. num_iters {
+        // let num_elems_range = Range::<usize>::new(1000, 1000000);
+        // let num_elems = num_elems_range.ind_sample(&mut rng);
+        let elems = gen_elems(num_elems, &mut rng);
+        let mins = calc_mins(&elems, num_mins);
+        let approx_size = approx_size_harmonic(&mins);
+
+        total_serror += (approx_size - elems.len()).pow(2);
+    }
+
+    let variance = total_serror / num_iters;
+
+    println!("variance = {}", variance);
+    println!("err ratio = {}", (variance as f64).sqrt() / num_elems as f64);
+
+
 }
