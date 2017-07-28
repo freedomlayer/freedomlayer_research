772a7acabd9f0e216469d9d811c2b7b0c2fba97e
real <real@freedomlayer.org>
2017-07-28 17:17:45 +0300

    Added other approximation functions and put them together inside a static array (With names).

--- approximate_net/src/approx_funcs.rs
+++ approximate_net/src/approx_funcs.rs
@@ -1,3 +1,12 @@
+use std;
+
+/// Calculate mean of values.
+fn mean(vals: &[f64]) -> f64 {
+    let amount = vals.len() as f64;
+    vals.iter()
+        .map(|&x| x / amount)
+        .sum()
+}
 
 /// Calculate harmonic mean of given values
 fn harmonic_mean(vals: &[f64]) -> f64 {
@@ -8,7 +17,16 @@ fn harmonic_mean(vals: &[f64]) -> f64 {
     (vals.len() as f64) / fsum
 }
 
-pub fn approx_size_harmonic(mins: &[u64]) -> usize {
+pub fn approx_size_harmonic_before(mins: &[u64]) -> usize {
+
+    let fmeans = mins.iter()
+        .map(|&m| m as f64)
+        .collect::<Vec<f64>>();
+    let hmean_min = harmonic_mean(&fmeans);
+    (((u64::max_value() as f64)/ hmean_min) as usize) - 1
+}
+
+pub fn approx_size_harmonic_after(mins: &[u64]) -> usize {
     let trans = mins.iter()
         .map(|&m| (u64::max_value() / m) - 1)
         .map(|x| x as f64)
@@ -16,6 +34,35 @@ pub fn approx_size_harmonic(mins: &[u64]) -> usize {
 
     harmonic_mean(&trans) as usize
 }
+
+pub fn approx_size_mean_before(mins: &[u64]) -> usize {
+    let fmeans = mins.iter()
+        .map(|&m| m as f64)
+        .collect::<Vec<f64>>();
+    let mean_min = mean(&fmeans);
+    (((u64::max_value() as f64)/ mean_min) as usize) - 1
+
+}
+
+pub fn approx_size_mean_after(mins: &[u64]) -> usize {
+    let trans = mins.iter()
+        .map(|&m| (u64::max_value() / m) - 1)
+        .map(|x| x as f64)
+        .collect::<Vec<f64>>();
+
+    mean(&trans) as usize
+}
+
+pub type ApproxFunc = &'static (Fn(&[u64]) -> usize +  std::marker::Sync);
+
+pub static APPROX_FUNCS_NAMED: &[(ApproxFunc, &'static str)] = 
+    &[
+        (&approx_size_harmonic_before, "approx_size_harmonic_before"), 
+        (&approx_size_harmonic_after, "approx_size_harmonic_after"),
+        (&approx_size_mean_before, "approx_size_mean_before"),
+        (&approx_size_mean_after, "approx_size_mean_after"),
+    ];
+
 
 #[cfg(test)]
 mod tests {
